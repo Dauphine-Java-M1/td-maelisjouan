8438779cee13b81ffbb4ea60bff6598a3dff8fe8
maelisjouan <maelis.kerve@gmail.com>
2019-10-15 14:29:55 +0200

    add serialVersionUID

--- shapes/src/main/java/fr/dauphine/ja/jouandekervenoaelmaelis/shapes/BrokenLine.java
+++ shapes/src/main/java/fr/dauphine/ja/jouandekervenoaelmaelis/shapes/BrokenLine.java
@@ -3,7 +3,12 @@ package fr.dauphine.ja.jouandekervenoaelmaelis.shapes;
 import java.util.LinkedList;
 
 public class BrokenLine extends LinkedList<Point>{
-	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1321468283894529908L;
+
 	public BrokenLine(){
 		super();
 	}
